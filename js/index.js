a1471ee7130c3e0ab141ad8993fad3b4e5db79c7
seoulomon <70969154+seoulomon@users.noreply.github.com>
2021-03-30 01:22:09 -0700

    figured out "If else" statements and applied it to the black and white image

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
 var $bwimg = $('.workigBW');
 var $win = $ (window);
+var $colorimg = $('.workImgCOLOR');
 
 $win.on('scroll', function() {
 //  console.log($win.scrollTop());
  var top = $win.scrollTop() /7;
  
  $bwimg.css('transform','translate(' + top +   'px )');
-    if ($win.scrollTop() == 954) {
-        console.log('954');
+    if ($win.scrollTop() >= 954) {
+        // console.log('954');
+        $('.workigBW').css('display', 'none');
+        
     }
+$colorimg.css('display','absolute');
+    if ($win.scrollTop() < 954) {
+        // console.log('954');
+        $('.workImgCOLOR').hide();}
+        
+    else {($win.scrollTop() >= 954) 
+        // console.log('954');
+        $('.workImgCOLOR').show();
+        
+        
+    }    
 
 
 });
